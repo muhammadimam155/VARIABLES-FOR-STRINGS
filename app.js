b55c2061b04557232d189dc4d80784aad170135b
muhammadimam155 <imamaliatari@gmail.com>
2023-09-25 00:19:08 +0500

    variables for strings

---
+++ app.js
@@ -0,0 +1,14 @@
+ var myName = "my name is waqas hussain"
+var myAge = "34 years old"
+var course = "Certified Mobile Application Development"
+var pizza = "pizza\npizz\npiz\npi\np"
+var email = "my email address is www@example.com"
+var education = "I am trying to learn from the book A smarter way to learn JavaScript"
+alert(myName);
+alert(myAge);
+alert(course);
+alert(pizza);
+alert(email);
+alert(education);
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+ 
